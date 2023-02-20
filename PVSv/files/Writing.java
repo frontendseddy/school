416fcd33983d6d874d9ec39063cc901a64b8d912
frontendseddy <pan.bramboru@seznam.cz>
2023-02-20 10:14:12 +0100

    delete "neco" and another

--- PVSv/files/Writing.java
+++ PVSv/files/Writing.java
@@ -7,7 +7,11 @@ import java.io.IOException;
 public class Writing {
     public static void main(String[] args) throws IOException {
         DataExport de = new DataExport("filesResources\\output\\out.txt");
-        
+        de.writeLine("New line, new text");
+        de.writeString("Hello");
+        de.writeString(" World!");
+        de.writeNewLine();
+        de.writeString("And another niew line");
         de.finishExport();
     }
 }
