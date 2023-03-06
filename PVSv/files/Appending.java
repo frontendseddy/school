560d47799c602b772c2ad33ad887be04c308933d
frontendseddy <pan.bramboru@seznam.cz>
2023-03-06 12:59:38 +0100

    add

--- PVSv/files/Appending.java
+++ PVSv/files/Appending.java
@@ -1,13 +1,23 @@
 package PVSv.files;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Appending {
     public static void main(String[] args) throws IOException {
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(new File("filesResources\\output\\AppendingOut.txt"))));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(new File("filesResources\\output\\AppendingOut.txt"), true)));
         for (int i = 0; i < 100; i++) {
-            pw.println("line num " + (i+1)) ;
+            pw.write("line num " + (i+1)); ;
         }
         pw.close();
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+        pw = new PrintWriter(new BufferedWriter(new FileWriter(new File("filesResources\\output\\AppendingOut.txt"), true)));
+        for (int i = 100; i < 200; i++) {
+            pw.println("line num:" + (i+1) + "\n");
+        }
+        pw.close();
+        System.out.println("Appended another 100 lines");
     }
 }
