47bd48efc176ff0f6c2ee792f855db3f6f3b64c5
frontendseddy <pan.bramboru@seznam.cz>
2023-04-04 10:43:25 +0200

    add

---
+++ PVSv/files/ExceptionsBasic.java
@@ -0,0 +1,21 @@
+package PVSv.files;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ExceptionsBasic {
+    public static void main(String[] args) throws IOException {
+        int[] arr = {1,24,79,87};
+        System.out.println(arr[1]);
+//        FileReader fr = new FileReader("won't find me.txt");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("zadej cislo");
+//        System.out.println(sc.nextInt());
+//        int a = 8 / 0;
+//        String a = null;
+//        System.out.println(a.toLowerCase());
+        System.out.println("Happy ending");
+    }
+}
