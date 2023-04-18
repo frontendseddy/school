463e33daf35bb3c7391ddb78db8ec48b29db84f6
frontendseddy <pan.bramboru@seznam.cz>
2023-04-18 11:24:07 +0200

    comparator mp3.java

--- XPVs/interfacesxd/MP3.java
+++ XPVs/interfacesxd/MP3.java
@@ -11,27 +11,37 @@ import java.util.List;
 
 public class MP3 {
     static ArrayList<Track> loadTracks(String path) {
+        ArrayList<Track> tracks = new ArrayList<>();
         try {
             List<String> lines = Files.readAllLines(Paths.get(path));
-            ArrayList<Track> tracks = new ArrayList<>();
             Track t;
             String[] attributes;
             for (String line : lines) {
                 attributes = line.split(";");
                 t = new Track(attributes[0], Integer.parseInt(attributes[1]), Double.parseDouble(attributes[2]), Integer.parseInt(attributes[3]));
                 tracks.add(t);
-            }return tracks;
+            }
         } catch (IOException e) {
             System.out.println("Could not read file");
             return null;
         }
+        return tracks;
     }
 
     public static void main(String[] args) {
         ArrayList<Track> tracks = loadTracks("Tracks.txt");
-        Collections.sort(tracks, new CompareTracksByYear());
+        Collections.sort(tracks, Track.BY_YEAR.reversed()); //bez reversed to bude vzestupne
         for (Track t: tracks) {
             t.printTrack();
+        }
+        Collections.sort(tracks, Track.BY_NAME.reversed()); //bez reversed to bude vzestupne
+        for (Track t: tracks) {
+            t.printTrack();
+        }
+        System.out.println("---TOP 10---");
+        Collections.sort(tracks, Track.BY_RAITING.reversed());
+        for (int i = 0; i < 10; i++) {
+            tracks.get(i).printTrack();
         }
 
     }
@@ -44,9 +54,24 @@ class Track implements Comparable<Track>{
     double rating;
     int duration;
 
-//    public static final Comparator<Track> BY_YEAR = new CompareTracksByYear() {
-//
-//    }
+    public static final Comparator<Track> BY_YEAR = new Comparator<Track>() {
+        @Override
+        public int compare(Track o1, Track o2) {
+            return o1.yearOfRelease - o2.yearOfRelease; //Integer.compare(o1.yearOfRelease, o2.yearOfRelease);
+        }
+    };
+    public static final Comparator<Track> BY_RAITING = new Comparator<Track>() {
+        @Override
+        public int compare(Track o1, Track o2) {
+            return Double.compare(o1.rating, o2.rating);
+        }
+    };
+    public static final Comparator<Track> BY_NAME = new Comparator<Track>() {
+        @Override
+        public int compare(Track o1, Track o2) {
+            return o1.name.compareTo(o2.name);
+        }
+    };
 
     public Track(String name, int yearOfRelease, double rating, int duration) {
         this.name = name;
