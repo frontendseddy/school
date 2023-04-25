2d30a6798a0887169ed41064d7e4dd01997f65bd
frontendseddy <pan.bramboru@seznam.cz>
2023-04-25 07:56:23 +0200

    something adding to BasicTime

--- PVSv/files/time/BasicTime.java
+++ PVSv/files/time/BasicTime.java
@@ -2,23 +2,31 @@ package PVSv.files.time;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class BasicTime {
     public static void main(String[] args) {
-        LocalDate someTime = LocalDate.now().minusDays(1);
+//        LocalDate someTime = LocalDate.of(2023, 4, 24);
+//        someTime.isLeapYear(); //prestupny rok
+//        System.out.println(someTime.getDayOfMonth());
+//        System.out.println(someTime.getDayOfWeek());
+//        System.out.println(someTime.getMonthValue());
+//        System.out.println(someTime.getMonth());
+//        System.out.println(someTime.getDayOfYear());
+//        System.out.println(someTime.getYear());
+//
+//        if (someTime.getDayOfWeek() == DayOfWeek.SATURDAY || someTime.getDayOfWeek() == DayOfWeek.SUNDAY) {
+//            System.out.println("Is weekend");
+//        } else {
+//            System.out.println("Is not a weekend");
+//        }
 
-        System.out.println(someTime.getDayOfMonth());
-        System.out.println(someTime.getDayOfWeek());
-        System.out.println(someTime.getDayOfYear());
-        System.out.println(someTime.getMonth());
-        System.out.println(someTime.getMonthValue());
-        System.out.println(someTime.getYear());
-
-        if (someTime.getDayOfWeek() == DayOfWeek.SATURDAY || someTime.getDayOfWeek() == DayOfWeek.SUNDAY) {
-            System.out.println("Is weekend");
-        }
-        else {
-            System.out.println("Is not a weekend");
-        }
+        LocalTime clock = LocalTime.now();
+        System.out.println(clock.toString());
+        System.out.println(clock.getHour());
+        System.out.println(clock.getMinute());
+        System.out.println(clock.getSecond());
+        System.out.println(clock.isBefore(LocalTime.MIDNIGHT));
+        System.out.println(clock);
     }
 }
