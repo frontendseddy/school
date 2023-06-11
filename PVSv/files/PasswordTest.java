2761fdc51aa7b3e0b39956ace9f79674b4e1d5a6
frontendseddy <pan.bramboru@seznam.cz>
2023-06-11 21:01:27 +0200

    learning to test

--- PVSv/files/PasswordTest.java
+++ PVSv/files/PasswordTest.java
@@ -9,17 +9,11 @@ public class PasswordTest {
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("filesResources\\passwords.txt"));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(new File("filesResources\\output\\PasswordOut.txt"))));
-//        for (String line : lines) {
-//            if (line.length() >= 6) {
-//                pw.println(line + " good");
-//            }
-//            else {
-//                pw.println(line + " not good");
-//            }
-//        }
         for (String line : lines) {
             if (line.length() >= 6) {
-                pw.println(line);
+                pw.println(line + " good");
+            } else {
+                pw.println(line + " not good");
             }
         }
         pw.close();
